a01a25bb2d25d4ca60d8042e42d80169e5b9ff1d
GitOGraphr512 <158799940+GitOGraphr512@users.noreply.github.com>
2024-08-25 18:49:26 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # My dwm install script
 -----------------------
-prerequisites below:
-xorg, Xinerama, coreutils, a modern shell (zsh, fish, bash (not sh))
+prerequisites:
+xorg, Xinerama, make, sudo, a modern shell (preferably bash), JetBrainsMono Nerd Font
 
 consult your distro's docs for installation instructions
+
+To, for example, have a background persistant across dwm restarts, you must edit your ~/.xinitrc. With a background program of your choosing, you could edit the default ~/.xinitrc to be like this:
+exec dwm &
+slstatus &
+'background command'
+The & is critical. If not included at the end of each line as shown, only the first command, 'exec dwm', will run. You would not have a bar or a background.
+
+It is highly recommended to start dwm via a tty with 'startx'. I have not and am not planning to test this dwm config using and display managers such as GDM, SDDM, or Ly.
--- dwminstall.sh
+++ dwminstall.sh
@@ -4,7 +4,7 @@ echo "#################"
 echo "# dwminstall.sh #"
 echo "#################"
 
-echo "Please check the README.md and read the prerequisites list. Install them if needed."
+echo "Please check the README.md and read the prerequisites list. Make sure they are installed."
 echo "Starting in 5 seconds..."
 echo "5"
 sleep 1
@@ -18,19 +18,22 @@ echo "1"
 sleep 1
 echo "0"
 
-echo "!!! DOAS USERS !!! Sudo is used in this script! Alias doas to sudo or install sudo before continuing! Ctrl-C to exit the script now and apply the changes mentioned."
-echo "Script will continue in 15 seconds."
-sleep 15
+echo "!!! DOAS USERS !!! Sudo is used in this script! Install sudo before continuing! Ctrl-C to exit the script now."
+echo "Script will continue in 5 seconds."
+sleep 5
 
 if [ -d ~/suckless ]; then
-	echo "~/suckless exists. Continuing..."
+	echo "~/suckless exists. Continue."
 else
 	echo "~/suckless does not exist, create it."
 	echo "mkdir ~/suckless"
 	mkdir ~/suckless
 fi
 
+sleep 2
+
 echo "Cloning repositories..."
+sleep 2
 pushd ~/suckless
 git clone https://github.com/GitOGraphr512/dwmdots.git
 git clone https://github.com/GitOGraphr512/stdots.git
@@ -39,11 +42,11 @@ git clone https://git.suckless.org/dmenu
 popd
 echo "Done!"
 
-echo "Building dwm and suckless tools..."
-
+echo "Building dwm and tools..."
+sleep 2
 pushd ~/suckless/dwmdots
 mkdir patches
-mv dwm-always-center-20200625-f04cac6.diff dwm-uselessgap-20211119-58414bee958f2.diff dwm-attachbelow-6.2.diff patches/
+mv dwm-alwayscenter-20200625-f04cac6.diff dwm-uselessgap-20211119-58414bee958f2.diff dwm-attachbelow-6.2.diff patches/
 sudo make clean install
 popd
 
@@ -59,10 +62,29 @@ sudo make clean install
 popd
 
 pushd ~/suckless/slstatusdots
+mkdir components
+cp battery.o cpu.o datetime.o disk.o entropy.o hostname.o ip.o kernel_release.o keyboard_indicators.o keymap.o load_avg.o netspeeds.o num_files.o ram.o run_command.o swap.o temperature.o uptime.o user.o volume.o wifi.o battery.c cpu.c datetime.c disk.c entropy.c hostname.c ip.c kernel_release.c keyboard_indicators.c keymap.c load_avg.c netspeeds.c num_files.c ram.c run_command.c swap.c temperature.c uptime.c user.c volume.c wifi.c components/
 sudo make clean install
 popd
-
 echo "Done!"
-echo "dwm, st, dmenu, and slstatus should be installed if everything went correctly."
-echo "If you find any bugs in this script, email me at <gitMailbox123@protonmail.com>."
-echo "Thanks for using my install script :-)"
+sleep 2
+
+echo "Cleaning up..."
+sleep 2
+pushd ~/suckless/slstatusdots
+rm battery.o cpu.o datetime.o disk.o entropy.o hostname.o ip.o kernel_release.o keyboard_indicators.o keymap.o load_avg.o netspeeds.o num_files.o ram.o run_command.o swap.o temperature.o uptime.o user.o volume.o wifi.o battery.c cpu.c datetime.c disk.c entropy.c hostname.c ip.c kernel_release.c keyboard_indicators.c keymap.c load_avg.c netspeeds.c num_files.c ram.c run_command.c swap.c temperature.c uptime.c user.c volume.c wifi.c
+popd
+echo "Done!"
+
+echo "Creating a simple ~/.xinitrc..."
+touch ~/.xinitrc
+echo "exec dwm &" > ~/.xinitrc
+echo "slstatus" >> ~/.xinitrc
+echo "Done!"
+echo "Note: Starting dwm with 'startx' through a tty is HIGHLY recommended. I am not planning on testing this dwm config with any display managers such as SDDM or Ly, so I have no idea as to if this config works correctly with DMs."
+echo "Note: The README.md explains how to edit the ~/.xinitrc file to your needs. Read it before asking: 'How do I add a persistant background?' or 'How do I set my brightness to x% at dwm startup?'"
+
+echo "================================="
+echo "dwm and tools should be installed if everything went correctly."
+echo "If you find any bugs in this script or have any questions, comments, or concerns, email me at <gitMailbox123@protonmail.com>, or contact me on Jami: <gentoouser420>"
+echo "Thanks for using my dwm install script :) - GitOGraphr512"
